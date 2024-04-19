0c10e3a3ec374542bc745d6c2098a44a15cb8562
abubienczyk <olabubienczyk@gmail.com>
2024-04-19 14:56:52 +0200

    comit

---
+++ WarehouseApp/WarehouseApp/Properties/Controllers/WarehouseController.cs
@@ -0,0 +1,27 @@
+namespace WarehouseApp.Properties.Controllers;
+
+public class WarehouseController
+{
+    // robic na taskach !!
+    //wstrzykiwianie zaleznosci --> dependency injection
+    
+    //ExectuteScalar -> zwraca obkiet lub null , jedna kolumna 
+    
+    
+    //pobieranie danych --> wazne bedzie na kolosie
+    //zamiast kluczy obcych zwracamy oelny obiekt 
+    
+    //jesli takie same nazwy kolumn to zrobic aliasy np name AS productName
+    
+    // jsk tworzymy obiekt to zwracamy DTO
+    
+    //haslo yourStrong@Password
+    
+    // TransactionScope - w jakim zakresie ma zialac transakcja 
+    
+    //zadanie 2 zdjecie
+    
+    // laczenie z baza manuall
+    // name SA paswor yourStrong@Password
+    
+}
