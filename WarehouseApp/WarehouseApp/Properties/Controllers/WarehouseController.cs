c2feb44466223598a17d2cc55ddc0ce4c8d0f4cc
abubienczyk <olabubienczyk@gmail.com>
2024-04-24 12:19:07 +0200

    final

--- WarehouseApp/WarehouseApp/Properties/Controllers/WarehouseController.cs
+++ WarehouseApp/WarehouseApp/Properties/Controllers/WarehouseController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using WarehouseApp.Properties.Models;
+using WarehouseApp.Properties.Models.DTO_s;
 using WarehouseApp.Properties.Repositories;
 
 namespace WarehouseApp.Properties.Controllers;
@@ -11,6 +13,40 @@ public class WarehouseController : ControllerBase
     public WarehouseController(IWarehouseRepository warehouseRepository)
     {
         _warehouseRepository =warehouseRepository;
+    }
+    [HttpPost]
+    public async Task<IActionResult> AddProdcut(InsertIntoProduct_WarehouseDTO p)
+    {
+        //punkt 1 -> czy istnieje magazyn czy istnieje produkt i czy amount>0
+        if (!await _warehouseRepository.DoesProductExists(p.IdProduct))
+            return NotFound();
+        if (!await _warehouseRepository.DoesWarehouseExists(p.IdWarehouse))
+            return NotFound();
+        if (p.Amount <= 0)
+            return BadRequest();
+        //punkt 2 --> czy jest takie zamowienie z id produktu i amount i czy data<cretedOredr
+        if (!await _warehouseRepository.DoesOrderExists(p.IdProduct, p.Amount, p.CreatedAt))
+            return NotFound();
+        
+        //punkt 3 czy jest takie zamowienie w warehouse
+        int id = await _warehouseRepository.GetOrderId(p.IdProduct);
+        if (await _warehouseRepository.WasOrderRealized(id))
+            return Conflict();
+        //punkt 4 aktualizacja Fullfiledat
+        await  _warehouseRepository.UpdateDate(id);
+        //punkt 5 wstawienie nowego produktu
+        double cena = await _warehouseRepository.GetPrice(p.IdProduct);
+        cena *= p.Amount;
+        
+        int newID=await _warehouseRepository.InsertIntoProduct_Warehouse(p, cena, id);
+        return Ok(newID);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddProdcutWithProcedure(InsertIntoProduct_WarehouseDTO p)
+    {
+        var index = await _warehouseRepository.InsertIntoProduct_Warehouse_With_Procedure(p);
+        return Ok(index);
     }
     // robic na taskach !!
     //wstrzykiwianie zaleznosci --> dependency injection
--- WarehouseApp/WarehouseApp/Properties/Repositories/IWarehouseRepository.cs
+++ WarehouseApp/WarehouseApp/Properties/Repositories/IWarehouseRepository.cs
@@ -6,9 +6,12 @@ public interface IWarehouseRepository
 {
     Task<bool> DoesProductExists(int id);
     Task<bool> DoesWarehouseExists(int id);
-    Task<bool> DoesOredrExists(int id);
-    Task<AddProductDTO> AddProduct(AddProductDTO product);
+    Task<bool> DoesOrderExists(int id, int amount, DateTime created);
+    Task<int> GetOrderId(int id);
     Task<bool> WasOrderRealized(int id);
     Task UpdateDate(int id);
-    Task<InsertIntoProduct_WarehouseDTO> InsertIntoProduct_Warehouse(InsertIntoProduct_WarehouseDTO data);
+    Task<double> GetPrice(int id);
+    Task<int> InsertIntoProduct_Warehouse(InsertIntoProduct_WarehouseDTO data, double cena, int id);
+    
+    Task<int> InsertIntoProduct_Warehouse_With_Procedure(InsertIntoProduct_WarehouseDTO data);
 }
